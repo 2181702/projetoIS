81aba028cf4ca696867aa944d0d2cf1908fd0222
David Batista <2181218@my.ipleiria.pt>
2020-11-27 17:37:56 +0000

    Write Json to HTML Works!

--- ClientForm/HtmlHandler.cs
+++ ClientForm/HtmlHandler.cs
@@ -23,10 +23,12 @@ namespace ClientForm
                 {
                     using (StreamWriter w = new StreamWriter(fs, Encoding.UTF8))
                     {
-                        //w.WriteLine("<!DOCTYPE html>\n<html>\n<body>\n<div id='root'>");
-                        //w.WriteLine("</div>\n</body>\n</html>");
-                        //w.WriteLine("<script>document.getElementById('root').innerText =JSON.parse(" + json + ") </script>");
-                        w.WriteLine(json);
+                        w.WriteLine("<!DOCTYPE html>\n<html>\n<body>\n\t<div id='root'>");
+                        w.WriteLine("\t</div>\n</body>\n</html>");
+                        w.WriteLine("<script src= 'https://cdn.rawgit.com/caldwell/renderjson/master/renderjson.js'></script>");
+                         w.WriteLine("<script>const data = JSON.parse('" + json + "'); console.log(data); document.getElementById('root').appendChild(renderjson(data)) </script>");
+                        w.WriteLine("<script src='/path/to/dist/jsonview.bundle.js'></script>");
+                        //w.WriteLine(json);
                     }
                 }
                 return new Response<string>(json, "HTML created!", STATUS_CODE.OK);
